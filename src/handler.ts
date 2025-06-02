f07860b237b32a287f4c5e3f74e04fa8a673d322
maxburkanov <ostu.student@gmail.com>
2025-06-02 01:02:39 -0400

    feat(users) add environment

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { createUser, getUsers } from './userController';
 
+const env = process.env.API_ENV;
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  if (event.path === 'api/v1/users' && event.httpMethod === 'POST') {
+  if (event.path === `${env}api/v1/users` && event.httpMethod === 'POST') {
     return await createUser(event);
   }
 
-  if (event.path === 'api/v1/users' && event.httpMethod === 'GET') {
+  if (event.path === `${env}api/v1/users` && event.httpMethod === 'GET') {
     return await getUsers();
   }
 
