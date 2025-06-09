2019148831a885649fe6609d56f2d7cd79be6d7a
maxburkanov <ostu.student@gmail.com>
2025-06-08 20:56:12 -0400

    feat(auth) add db interaction

--- src/userController.ts
+++ src/userController.ts
@@ -3,16 +3,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const createUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const { name, email, passwordHash, phone } = JSON.parse(event.body || '{}');
-  // const res = await pool.query(
-  //   'INSERT INTO users (name, email, password_hash, phone) VALUES ($1, $2, $3, $4) RETURNING *',
-  //   [name, email, passwordHash, phone]
-  // );
+  const res = await pool.query(
+    'INSERT INTO users (name, email, password_hash, phone) VALUES ($1, $2, $3, $4) RETURNING *',
+    [name, email, passwordHash, phone]
+  );
 
-  // return {
-  //   statusCode: 201,
-  //   body: JSON.stringify({name, email, passwordHash, phone}),
-  // };
-  
   return {
     statusCode: 201,
     body: JSON.stringify({name, email, passwordHash, phone}),
@@ -20,15 +15,10 @@ export const createUser = async (event: APIGatewayProxyEvent): Promise<APIGatewa
 };
 
 export const getUsers = async (): Promise<APIGatewayProxyResult> => {
-  // const res = await pool.query('SELECT * FROM users');
-
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify(res.rows),
-  // };
+  const res = await pool.query('SELECT * FROM users');
 
   return {
     statusCode: 200,
-    body: JSON.stringify([{ name: 'John Doe', email: 'some@email.com', passwordHash: 'hashed-password', phone: '55555555' }]),
+    body: JSON.stringify(res.rows),
   };
 };
